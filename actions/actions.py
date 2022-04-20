154b364e828633d5c71c3838c30e9192b89376e3
Kev <100586415+k3vsteel@users.noreply.github.com>
2022-04-20 20:29:21 +0200

    combine actions with excel data
    - bug fixing with excel data and import
    - import has to be in action folder
    - combine excel data with custom actions for categories

--- actions/actions.py
+++ actions/actions.py
@@ -4,14 +4,15 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+# Import of Excel DB - Data
+from actions import dataImport
 
-# Import of Excel DB
-
-
-from typing import Any, Text, Dict, List
+# Rasa SDK Imports
+from typing import Any, Text, Dict, List, Tuple
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet
+from rasa_sdk.events import EventType
+from rasa_sdk.types import DomainDict
 
 
 class ActionFoodDirect(Action):
@@ -63,6 +64,27 @@ class ActionCarousel(Action):
                     ]
                 },
 
+                {
+                    "image": "https://image.jimcdn.com/app/cms/image/transf/dimension=890x10000:format=jpg"
+                             "/path/s08487bbdb796bdb9/image/i602f6404db8c165c/version/1614547865/rezepte-aus"
+                             "-essen-trinken.jpg",
+                    "title": "Leckeres Essen",
+                    "description": "Hier siehst du eines unserer Essen",
+                    "buttons": [
+                        {
+                            "title": "Zweites Beispiel",
+                            "payload": "/choose_food",
+                            "type": "postback"
+                        },
+
+                        {
+                            "title": "Hallo",
+                            "payload": "/next_food",
+                            "type": "postback"
+                        }
+                    ]
+                },
+
             ]
         }
         dispatcher.utter_message(json_message=carousel)
@@ -87,4 +109,39 @@ class ActionNoAdvice(Action):
         dispatcher.utter_message(text="Was du brauchst meine Hilfe gar nicht, dann vielleicht beim nächsten Mal!",
                                  image="https://img.freepik.com/vektoren-kostenlos/netter-box-charakter-laeuft_161751"
                                        "-1640.jpg")
+        return []
+
+
+# TODO: Slots and Forms
+class ActionChooseCategory(Action):
+    # return the name of the action - pay attention to the spelling
+    def name(self) -> Text:
+        return "decision_of_category"
+
+    # collectingDispatcher allows to send back messages to the user
+    def run(
+            self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        dispatcher.utter_message(
+            Text="Bitte wähle eine Essensrichtung oder klicke auf weiter:",
+            buttons=[{"title": i, "payload": i} for i in dataImport.categories.Bezeichnung]
+        )
+        return []
+
+
+class ActionAskForVegetarian(Action):
+
+    def name(self) -> Text:
+        return "ask_for_veg"
+
+    def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        dispatcher.utter_message(
+            Text="Bist du Vegetarier, Vegan oder Allesesser?:",
+            buttons=[{"payload": "/eat_all", "title": "Allesesser"},
+                     {"payload": "/vegan", "title": "Vegan"},
+                     {"payload": "/vegetarian", "title": "Vegetarier"}])
         return []
